c9d41f5d7b954d49d5b7f168c217ac1929a94346
Ben <benrgreene@gmail.com>
2025-06-19 16:34:29 -0700

    update search bar styles

--- snippets/layout/global/search.php
+++ snippets/layout/global/search.php
@@ -2,11 +2,15 @@
 	<form search-box
 		  class="search-bar"
 		  method="GET"
-		  action="<?= get_site_url() ?>"
-		  is-ajax>
-		<div class="ccontain">
-			<input search-input name="s" type="text" />
-			<input type="submit" value="Search" />
+		  action="<?= get_site_url() ?>">
+		<div class="ccontain search-bar__input-wrap">
+			<label for="s">Search Term</label>
+			<input search-input name="s" id="s" type="text" />
+			<input type="submit" value="Search" class="button" />
 		</div>
 	</form>
+	<button class="search-bar__overlay"
+			onclick="window.setState('searchOpen', false)"
+			aria-label="Close Search Bar">		
+	</button>
 </html-if>
