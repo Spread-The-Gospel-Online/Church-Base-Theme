a34e93ee8d9226aff90eecc3bd6d96b004a23c31
Ben <benrgreene@gmail.com>
2025-07-15 12:28:08 -0700

    fix permalinks for search

--- core/utilities/permalinks.php
+++ core/utilities/permalinks.php
@@ -11,12 +11,19 @@ function church_get_single_url ($type, $slug, $prefix = '/') {
 	return $prefix . $type . '/' . $slug;
 }
 
-// TODO: figure out what i was drinking when i wrote this b/c wow it must have been really good.
 function church_get_general_url ($type, $slug = false, $prefix_type = false, $prefix_slug = false) {
 	$prefix = $prefix_type ? sprintf('/$s/$s/', $prefix_type, $prefix_slug) : '/';
 	$post = get_posts(array('name' => $slug, 'post_type' => $type));
+	if ($post) {
+		$text = $post[0]->post_title;
+	} else if ($type == 'search') {
+		$text = 'Search Results';
+	} else {
+		$text = 'All ' . ucwords(str_replace('-', ' ', $type));
+	}
+
 	return array(
 		'url' => $slug ? church_get_single_url($type, $slug, $prefix) : church_get_archive_url($type, $prefix),
-		'text' => $slug && $post ? $post[0]->post_title : 'All ' . ucwords(str_replace('-', ' ', $type))
+		'text' => $text
 	);
 }
