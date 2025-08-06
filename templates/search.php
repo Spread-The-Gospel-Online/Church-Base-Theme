02e5f82b6adc69c629bb0768697acf25b9809822
Ben <benrgreene@gmail.com>
2025-08-06 14:57:38 -0700

    update search hero

--- templates/search.php
+++ templates/search.php
@@ -2,16 +2,18 @@
 
 get_header();
 
+// Get content for hero
+$front_page_id = get_option('page_on_front');
+$front_page = get_page($front_page_id);
+$image = util_get_hero_src($front_page);
+
+// Build search results query
 $search_query = get_search_query();
 $args = array(
 	's' => $search_query,
 	'paged' => (get_query_var('paged')) ? get_query_var('paged') : 1
 );
 $the_query = new WP_Query($args);
-
-$front_page_id = get_option('page_on_front');
-$front_page = get_page($front_page_id);
-$image = util_get_hero_src($front_page);
 
 util_render_snippet('common/hero', array(
 	'desktop_src' => $image['src'],
@@ -24,6 +26,8 @@ util_render_snippet('common/hero', array(
 	'text_color' => get_post_meta($front_page_id, 'hero_text', true),
 	'hero_height' => get_post_meta($front_page_id, 'hero_height', true),
 ), false);
+
+do_action('church_layout_after_header');
 
 
 util_render_snippet('layout/search/search-page', array(
